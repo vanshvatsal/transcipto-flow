28091fd2ddf0ab26001d3c62fdd9772fda84b4b3
Vansh Vatsal <114556099+vanshvatsal@users.noreply.github.com>
2024-07-30 23:50:54 +0530

    commit initial

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect, useRef } from 'react'
 
 export default function HomePage(props) {
     const { setAudioStream, setFile } = props
-
     const [recordingStatus, setRecordingStatus] = useState('inactive')
     const [audioChunks, setAudioChunks] = useState([])
     const [duration, setDuration] = useState(0)
@@ -67,7 +66,7 @@ export default function HomePage(props) {
 
     return (
         <main className='flex-1  p-4 flex flex-col gap-3 text-center sm:gap-4  justify-center pb-20'>
-            <h1 className='font-semibold text-5xl sm:text-6xl md:text-7xl'>Free<span className='text-blue-400 bold'>Scribe</span></h1>
+            <h1 className='font-semibold text-5xl sm:text-6xl md:text-7xl'>Transcripto<span className='text-blue-400 bold'>Flow</span></h1>
             <h3 className='font-medium md:text-lg'>Record <span className='text-blue-400'>&rarr;</span> Transcribe <span className='text-blue-400'>&rarr;</span> Translate</h3>
             <button onClick={recordingStatus === 'recording' ? stopRecording : startRecording} className='flex specialBtn px-4 py-2 rounded-xl items-center text-base justify-between gap-4 mx-auto w-72 max-w-full my-4'>
                 <p className='text-blue-400'>{recordingStatus === 'inactive' ? 'Record' : `Stop recording`}</p>
@@ -82,7 +81,7 @@ export default function HomePage(props) {
                 const tempFile = e.target.files[0]
                 setFile(tempFile)
             }} className='hidden' type='file' accept='.mp3,.wave' /></label> a mp3 file</p>
-            <p className='italic text-slate-400'>Free now free forever</p>
+            <p className='italic text-slate-400'>Built for Use by anyone and everyone by Vansh Vatsal</p>
         </main>
     )
 }
--- src/components/Information.jsx
+++ src/components/Information.jsx
@@ -53,7 +53,7 @@ export default function Information(props) {
         const element = document.createElement("a")
         const file = new Blob([textElement], { type: 'text/plain' })
         element.href = URL.createObjectURL(file)
-        element.download = `Freescribe_${new Date().toString()}.txt`
+        element.download = `Transcripto_Flow_${new Date().toString()}.txt`
         document.body.appendChild(element)
         element.click()
     }
@@ -77,8 +77,9 @@ export default function Information(props) {
 
     return (
         <main className='flex-1  p-4 flex flex-col gap-3 text-center sm:gap-4 justify-center pb-20 max-w-prose w-full mx-auto'>
-            <h1 className='font-semibold text-4xl sm:text-5xl md:text-6xl whitespace-nowrap'>Your <span className='text-blue-400 bold'>Transcription</span></h1>
-
+            {tab === "transcription" ? <h1 className='font-semibold text-4xl sm:text-5xl md:text-6xl whitespace-nowrap'>Your <span className='text-blue-400 bold'>Transcription</span></h1>
+                : <h1 className='font-semibold text-4xl sm:text-5xl md:text-6xl whitespace-nowrap'>Your <span className='text-blue-400 bold'>Translation</span></h1>
+            }
             <div className='grid grid-cols-2 sm:mx-auto bg-white  rounded overflow-hidden items-center p-1 blueShadow border-[2px] border-solid border-blue-300'>
                 <button onClick={() => setTab('transcription')} className={'px-4 rounded duration-200 py-1 ' + (tab === 'transcription' ? ' bg-blue-300 text-white' : ' text-blue-400 hover:text-blue-600')}>Transcription</button>
                 <button onClick={() => setTab('translation')} className={'px-4 rounded duration-200 py-1  ' + (tab === 'translation' ? ' bg-blue-300 text-white' : ' text-blue-400 hover:text-blue-600')}>Translation</button>
